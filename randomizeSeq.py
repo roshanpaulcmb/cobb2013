3992a3713f5054674394539611baccf7a5aae772
roshanpaulcmb <rop174@pitt.edu>
2026-01-13 15:07:24 -0500

    Version 1.0.0

---
+++ randomizeSeq.py
@@ -0,0 +1,44 @@
+#### IMPORTS ####
+import pandas as pd
+import random
+
+
+#### FUNCTIONS ####
+
+def readData(file):
+    df = pd.read_csv(file)
+    return df
+
+
+def createRandomSeq(df):
+    
+    aas = df["code"]
+    probs = df["prob"]
+    
+    # generate random amino acid string based on likelihood of amino acid occuring in eukaryotes
+    randomSeq = random.choices(aas, weights = probs, k=100)
+    randomSeq = "".join(randomSeq)
+    return randomSeq
+
+def writeFasta(fileName, seq, header = None, lineWidth = 60):
+    with open(fileName, "w") as f:
+        f.write(f">{header}\n")
+        for i in range(0, len(seq), lineWidth):
+            f.write(seq[i:i+lineWidth] + "\n")
+    return None
+
+#### RUN ####
+
+def runAll(file):
+    df = readData(file)
+    randomSeqs = []
+    for i in range(10):
+        randomSeqs.append(createRandomSeq(df))
+        fileName = "random" + i + ".fasta"
+        writeFasta(fileName, randomSeqs[i])
+        print(i, randomSeqs[i])
+    return randomSeqs
+
+if __name__ == "__main__":
+    file = "aa_likeliness.csv"
+    runAll(file)
